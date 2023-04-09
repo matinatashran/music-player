18461a877febac854cb0b28a58bde1b4670b6dd3
matin atashran <matinatashran.5160@gmail.com>
2023-04-09 19:14:11 +0430

    Create a function by name 'createMusicList' for create list of musics after get files that uploaded.

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -2,8 +2,10 @@
 // Example: input => 190 / output => 03:10
 export function formatDuration(value) {
     const minutes = Math.floor(value / 60);
-    const seconds = (value - (minutes * 60)).toFixed(0);
-    return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    const seconds = (value - minutes * 60).toFixed(0);
+    return `${minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds < 10 ? `0${seconds}` : seconds
+    }`;
 }
 
 // ####################################
@@ -22,6 +24,22 @@ export async function getDuration(url) {
 // ####################################
 
 // This function shuffle an array and return it
-export function shuffle(array){
+export function shuffle(array) {
     return array.sort(() => Math.random() - 0.5);
+}
+
+// ####################################
+
+export async function createMusicList(selectedFiles) {
+    const files = [];
+    for (let i = 0; i < Object.keys(selectedFiles).length; i++) {
+        const url = URL.createObjectURL(selectedFiles[i]);
+        files.push({
+            id: i,
+            name: selectedFiles[i].name,
+            duration: await getDuration(url),
+            audio: new Audio(url),
+        });
+    }
+    return files;
 }
